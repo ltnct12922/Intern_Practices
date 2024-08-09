dd42195769ca177b25af9e68f12b6784abaea840
LeTinhNhut <ltnct12922@gmail.com>
2024-08-09 14:54:54 +0700

    Changes: OSPWA project

---
+++ OSPWA/src/main/java/com/example/OSPWA/OspwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.OSPWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OspwaApplication.class, args);
+	}
+
+}
---
+++ OSPWA/src/test/java/com/example/OSPWA/OspwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OSPWA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OspwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
